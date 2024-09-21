0cf287da2adee20b0e82336a1e2d6c3e99d16990
kabaruwka881 <killmepleasepliz@gmail.com>
2024-09-21 15:20:32 +1000

    update 22.09.2024

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ struct Node {
     int data;
     Node* next;
 
-    Node(int data, Node* nexct) {
+    Node(int data, Node* next) {
         this->data = data;
         this->next = next;
     }
@@ -77,6 +77,75 @@ void delete_list(Node*& head) {
     delete curr;
     head = nullptr;
     cout << endl;
+}
+
+bool search_list(Node* head, int key) {
+    if (head == nullptr) {
+        return false;
+    }
+    int count = count_list(head);
+    Node* temp = head;
+    for (int i = 0; i < count; i++) {
+        if (temp->data == key) {
+            cout << endl;
+            return true;
+        }
+        temp = temp->next;
+    }
+    cout << endl;
+    return false;
+}
+
+void delete_by_value(Node*& head, int key) {
+    if (head == nullptr) {
+        return;
+    }
+    else if (head->next == head) {
+        delete head;
+        head = nullptr;
+        return;
+    }
+    Node* temp = head;
+    Node* prev = nullptr;
+    int count = count_list(head);
+    while (temp->data == key) {
+        prev = temp;
+        temp = temp->next;
+        delete prev;
+        count--;
+    }
+    head = temp;
+    for (int i = 1; i < count; i++) {
+        if (temp->next->data == key) {
+            prev = temp->next;
+            temp->next = prev->next;
+            delete prev;
+        } else {
+            temp = temp->next;
+        }
+    }
+    temp->next = head;
+}
+
+void delete_after_by_value(Node*& head, int key) {
+    if (head == nullptr || head->next == head) {
+        return;
+    }
+    Node* temp = head->next;
+    Node* prev = head;
+    int count = count_list(head);
+    for (int i = 1; i < count; i++) {
+        if (prev->data == key) {
+            prev->next = temp->next;
+            delete temp;
+            count--;
+        }
+        prev = prev->next;
+        if (prev->next == head) {
+            break;
+        }
+        temp = prev->next;
+    }
 }
 
 int main() {
